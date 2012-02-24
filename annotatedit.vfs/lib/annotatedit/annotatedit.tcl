4c0b8f8e3a026f78b3f8ca5b10f8abe4fa234e1a
Jim DeVona <jim.devona@gmail.com>
2012-02-24 10:34:32 -0500

    A few notes planning rewrite of tagging loop.

--- annotatedit.vfs/lib/annotatedit/annotatedit.tcl
+++ annotatedit.vfs/lib/annotatedit/annotatedit.tcl
@@ -1,3 +1,8 @@
+#
+# Here is an intro comment block.
+# It is now two lines long.
+#
+
 package provide annotatedit 0.1
 namespace eval annotatedit {
 
@@ -7,8 +12,8 @@ namespace eval annotatedit {
 	package require text::sync
 
 	# create the editor widgets
-	ctext .anno -wrap none -height 20 -highlight 0 -fg "gray" 
-	ctext .code -wrap none -height 20 -highlight 0
+	ctext .anno -wrap none -height 20 -font TkFixedFont -highlight 0 -fg "gray" 
+	ctext .code -wrap none -height 20 -font TkFixedFont -highlight 0
 			
 	# put the widgets in the window
 	pack .anno .code -expand 1 -fill both -side left
@@ -33,18 +38,20 @@ namespace eval annotatedit {
 	
 	# Search for comment lines and tag them as ANNOtations.
 	# (This assumes there was some text worth searching inserted from the CLI.)
-	# Obviously, this one-time search is only feasible as an initial test.
-	# This tagjob will be used to work out different tag styles for each editor.
-	# (text::sync syncs tags, but we need to modify to NOT sync tag configure.)
-	# Once that's sorted, we can work on tagging during insertion, updating, etc.
-	.code tag configure ANNO -background "light gray"
+	.code tag configure ANNO -background "light gray" 
 	.anno tag configure ANNO -foreground "purple"
 	variable starts {}
 	variable spans {}
+	
+	# actually, not using -all would prob let do the search incrementally as part of
+	# the tagging loop, which will also make it easy to compute the intervening spans to tag as CODE
 	set starts [.code search -all -count ::annotatedit::spans -regexp {^\s#.*$} 1.0]
+	
+	set annoID 0
 	foreach start $starts span $spans {
 		# confirmed: increment span to include newline; if elided, hides line.
-		.code tag add ANNO $start "$start + [expr {$span + 1}] indices"
+		.code tag add ANNO $start "$start + [incr span] indices"
+		.code tag add [format "ANNO%d" [incr annoID]] $start "$start + $span indices"
+		puts [format "$annoID: %s" [.code tag ranges "ANNO$annoID"]]
 	}
-	
 }
